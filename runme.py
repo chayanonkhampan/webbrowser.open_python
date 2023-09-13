5fd0dd5d2dcf3107a7a552183344402f6e84358a
minecraft2048bitgame <121738550+chayanonkhampan@users.noreply.github.com>
2023-09-13 15:27:27 +0700

    Update runme.py

--- runme.py
+++ runme.py
@@ -1,2 +1,5 @@
-import webbrowser
-webbrowser.open("pranarai.ac.th")
+import webbrowser as wb
+wb.open("pranarai.ac.th")
+
+//หมายเหตุ สามารถเปลื่ยน url เป็นเว็บอื่นได้ในวงเล็บ
+//คำเตือน เว็บที่ใส่ไม่จำเป็นต้องเป็นเว็บที่ปลอดภัยสิ่งนี้สามารถใช้เพื่อเปิดเว็บที่อัตรายได้
